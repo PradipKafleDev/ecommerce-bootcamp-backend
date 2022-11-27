5d3966c5a6d1363751104ad9a5850230bb444b72
PradipKafleDev <pradipkafle074@gmail.com>
2022-11-27 18:21:12 +1100

    JWT TOKEN created and compared password

--- models/user.schema.js
+++ models/user.schema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles";
 import JWT from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import crypto from "crypto";
+import config from "../config";
 
 const userSchema = new mongoose.Schema(
   {
@@ -39,5 +39,26 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+//add more feature directly to schema
+userSchema.methods = {
+  //compare password
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+
+  //generate JWT TOKEN
+  getJWTToken: function () {
+    return JWT.sign(
+      {
+        _id: this._id,
+        role: this.role,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: config.JWT_EXPIRY,
+      }
+    );
+  },
+};
 
 export default mongoose.model("User", userSchema);
