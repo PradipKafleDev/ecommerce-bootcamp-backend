3653eb56c22bb4acb17c0a4a6cc933754da95ce1
PradipKafleDev <pradipkafle074@gmail.com>
2022-12-17 16:31:49 +1100

    added morgan and express middleware

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 
+//middleware
 const app = express();
+app.use(express.json());
+//url encoded to accept form data
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+//morgon logger
+app.use(morgan("tiny")); // prints information about api req and response in console
 export default app;
